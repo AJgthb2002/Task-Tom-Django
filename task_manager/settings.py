dadd2159b38d2832dccbb10f5ceeaf191e107d06
AnanyaJ <joshiananya20@gmail.com>
2022-03-01 09:40:27 +0530

    Initial commit

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT= BASE_DIR/"static"
+# STATIC_ROOT= BASE_DIR/"static"
 STATICFILES_DIRS=[BASE_DIR / "static"]
 STATIC_URL = '/static/'
 # STATIC_URL = '/static/'
